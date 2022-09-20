50311c1ef98dd1f3e856f6db6d54283d6040f43e
Audun <audunyoung@gmail.com>
2022-09-20 16:37:39 -0600

    Header file structure setup.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
+import PageHeader from './components/PageHeader/PageHeader';
 
 function App() {
   return (
     <div className="App">
+      <PageHeader />
     </div>
   );
 }
---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,12 @@
+import './PageHeader.scss';
+
+const PageHeader = () => {
+    render (
+        <div className='page-header'>
+            
+        </div>
+
+    )
+}
+
+export default PageHeader
