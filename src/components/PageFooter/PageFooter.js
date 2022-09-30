d292c6a78ebc3dc94679a6b15322d40aa0f3b614
Audun <audunyoung@gmail.com>
2022-09-30 15:33:01 -0600

    Create footer and initial styling.

--- src/components/PageFooter/PageFooter.js
+++ src/components/PageFooter/PageFooter.js
@@ -1,9 +1,16 @@
 import './PageFooter.scss';
+import instaIcon from '../../assets/icons/insta-icon.png';
 
 const PageFooter = () => {
+    let currentYear = new Date().getFullYear();
     return (
         <div className='footer'>
-
+            <div className='footer__text'>
+                <p className='footer__text-copyright'>&copy; MCC HOMES {currentYear}</p>
+            </div>
+            <div className='footer__socials'>
+                <img src={instaIcon} alt='instagram logo' className='footer__socials-insta' />
+            </div>
         </div>
     )
 }
