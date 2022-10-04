d3ccb0b4b5893d9ab4bcff790ce399124d478dba
Audun <audunyoung@gmail.com>
2022-10-04 11:18:17 -0600

    Set up projects page folder structure and files.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PageHeader from './components/PageHeader/PageHeader';
 import HomePage from './pages/HomePage/HomePage';
 import AboutPage from './pages/AboutPage/AboutPage';
 import ContactPage from './pages/ContactPage/ContactPage';
+import ProjectsPage from './pages/ProjectsPage/ProjectsPage';
 import PageFooter from './components/PageFooter/PageFooter';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' exact component={HomePage} />
           <Route path='/about' component={AboutPage} />
           <Route path='/contact' component={ContactPage} />
+          <Route path='/projects' component={ProjectsPage} />
         </Switch>
         <PageFooter />
       </div>
---
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -0,0 +1,14 @@
+import './ProjectsPage.scss';
+import { Component } from "react";
+
+class ProjectsPage extends Component {
+    render() {
+        return (
+            <div className='projects-page'>
+
+            </div>
+        )
+    }
+}
+
+export default ProjectsPage
