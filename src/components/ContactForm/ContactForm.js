f13e3ae6a032618a7a8097115a97f786801c576b
Audun <audunyoung@gmail.com>
2022-10-04 10:45:31 -0600

    Inital structure and styling for contact page.

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,25 @@
+import './ContactForm.scss';
+
+const ContactForm = () => {
+    return ( 
+        <div className='contact'>
+            <form className='contact__form'>
+                <div className='contact__form-split'>
+                    <div className='contact__form-split--left'>
+                        <label for='name' className='contact__form-label'>NAME *</label><br/>
+                        <input type='text' id='name' name='name' placeholder='John Doe' className='contact__form-input' />   
+                    </div>
+                    <div>
+                        <label for='email' className='contact__form-label'>EMAIL *</label><br/>
+                        <input type='text' id='email' name='email' placeholder='john.doe@gmail.com' className='contact__form-input' />  
+                    </div>
+                </div>
+                <label for='message' className='contact__form-label'>MESSAGE *</label><br/>
+                <textarea id='message' name='message' className='contact__form-input-mess' /><br/>
+                <input type='submit' value='SEND' className='contact__form-input-button' />
+            </form>
+        </div>
+    )
+}
+
+export default ContactForm;
--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,11 +1,28 @@
 import './ContactPage.scss';
 import { Component } from "react";
+import ContactForm from '../../components/ContactForm/ContactForm';
 
 class ContactPage extends Component {
     render() {
         return (
-            <div className='contact'>
-
+            <div className='contact-page'>
+                <h1 className='contact-page__title'>CONTACT US</h1>
+                <div className='contact-page__split'>
+                    <ContactForm />
+                    <div className='contact-page__split--right'>
+                        <div>
+                            <h2 className='contact-page__sub-head'>CALL US</h2>
+                            <h3 className='contact-page__body-text'>(435)654-0907</h3>
+                        </div>
+                        <div>
+                            <h2 className='contact-page__sub-head'>VISIT US</h2>
+                            <h3 className='contact-page__body-text'>599 N Main Street,<br/>Heber City, Utah 84032</h3>
+                        </div>
+                        <div>
+                            <h2 className='contact-page__sub-head'>FOLLOW US</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
