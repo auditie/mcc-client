6a71a05042c7b35879c90664ac5cc6233831b642
Audun <audunyoung@gmail.com>
2022-10-05 11:51:59 -0600

    Initial structure and styling for projects page and links.

--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -1,9 +1,16 @@
 import './ProjectLink.scss';
+import defaultImg from '../../assets/images/placeholders/house.jpg';
 
 const ProjectLink = () => {
     return (
-        <div className='project-link'>
-
+        <div className='project-link' style={{ backgroundImage: `url(${defaultImg})`,
+        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat' }}>
+            <div className='project-link__square' />
+            <img src={defaultImg} alt='house' className='project-link__img' />
+            <div className='project-link__overlay' />
+            <h3 className='project-link__title'>PROJECT NAME</h3>
         </div>
     )
 }
--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,13 +1,44 @@
 import './ProjectsPage.scss';
 import { Component } from "react";
 import ProjectsNav from '../../components/ProjectsNav/ProjectsNav';
+import ProjectLink from '../../components/ProjectLink/ProjectLink';
+import heroImg from '../../assets/images/heros/house4.png';
+import defaultImg from '../../assets/images/placeholders/house.jpg';
+
+// let backgroundStyle = {
+//     backgroundImage: `url(${defaultImg})`,
+//     backgroundPosition: 'center',
+//     backgroundSize: 'cover',
+//     backgroundRepeat: 'no-repeat'
+// };
 
 class ProjectsPage extends Component {
+    state = {
+        projects: [],
+        location: 'Colony'
+    }
+
+    // handleLocationSelect = id => {
+    //     this.setState({location: })
+    // }
+
     render() {
         return (
             <div className='projects-page'>
-                <div className='projects-page__hero'></div>
-                <ProjectsNav />
+                <div className='projects-page__hero' >
+                    <img src={heroImg} alt='house' className='projects-page__hero-img' />
+                </div>
+                <div className='projects-page__guide'>
+                    <ProjectsNav />
+                    <div className='projects-page__links'>
+                        <ProjectLink />
+                        <ProjectLink />
+                        <ProjectLink />
+                        <ProjectLink />
+                        <ProjectLink />
+                        <ProjectLink />
+                    </div>
+                </div>
             </div>
         )
     }
