494c8272972d474e8ff0c0a2bd368e58eab616a3
Audun <audunyoung@gmail.com>
2022-10-05 15:21:42 -0600

    Create admin login component and route.

--- src/pages/AdminPage/AdminPage.js
+++ src/pages/AdminPage/AdminPage.js
@@ -1,11 +1,15 @@
 import './SiteManager.scss';
 import { Component } from "react";
+import { Route, Switch } from 'react-router-dom';
+import AdminLogin from '../../components/AdminLogin/AdminLogin';
 
 class AdminPage extends Component {
     render() {
         return (
             <div className='admin-page'>
-
+                <Switch>
+                    <Route path='/admin' component={AdminLogin} />
+                </Switch>
             </div>
         )
     }
