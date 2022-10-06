910d13633bb4602ced695337e0848939f185cb28
Audun <audunyoung@gmail.com>
2022-10-06 15:22:46 -0600

    Create job sites list component files and route.

---
+++ src/components/JobSitesList/JobSitesList.js
@@ -0,0 +1,14 @@
+import './JobSitesList.scss';
+import { Component } from "react";
+
+class JobSitesList extends Component {
+    render() {
+        return (
+            <div className='job-sites'>
+
+            </div>
+        )
+    }
+}
+
+export default JobSitesList;
--- src/pages/AdminPage/AdminPage.js
+++ src/pages/AdminPage/AdminPage.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import AdminLogin from '../../components/AdminLogin/AdminLogin';
 import ManageSite from '../../components/ManageSite/ManageSite';
 import NewJob from '../../components/NewJob/NewJob';
+import JobSitesList from '../../components/JobSitesList/JobSitesList';
 
 class AdminPage extends Component {
     render() {
@@ -13,6 +14,7 @@ class AdminPage extends Component {
                     <Route path='/admin' exact component={AdminLogin} />
                     <Route path='/admin/manage' component={ManageSite} />
                     <Route path='/admin/new-job' component={NewJob} />
+                    <Route path='/admin/jobsites' component={JobSitesList} />
                 </Switch>
             </div>
         )
